42c098daec3b140e9abba21925943437141e4e57
ricken07 <ricken.bazolo@gmail.com>
2017-07-17 11:43:30 +0100

    fix: diff between night and day pharma into search result

--- js/adresses_search.js
+++ js/adresses_search.js
@@ -309,6 +309,20 @@ $(document).ready(function(){(false)
             content += "<a class='tel' href='tel:"+ results[i].tel1 +"'>"+ results[i].tel1 +"</a>";
             content += "&nbsp; <a class='tel' href='tel:"+ results[i].tel2 +"'>"+ results[i].tel2 +"</a>";
             content += "</div>";
+            content += "<div class='place_image_container'>";
+                  var pharmacyName = results[i].name.toLowerCase();
+                  if( pharmacyName.indexOf("nuit") == -1 ){
+                    //pharmacy de jour
+                    content += "<i class='fa fa-sun-o fa-2x' aria-hidden='true'></i>";
+                  } else {
+                    //pharmacie de nuit
+                    content += "<i class='fa fa-moon-o fa-2x' aria-hidden='true'></i>";
+                  }
+               content += "</div>";               
+               content += "</div>";
+
+ 
+           content += "</div>";
         };
         
         $("#results").html(content);
